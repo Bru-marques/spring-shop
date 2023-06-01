f636ec2c5d0802c87b4ee4977af9617d71cf9c1e
bru-marques <brmarques1108@gmail.com>
2023-06-01 18:13:48 +0100

    Setup authentication

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,4 +1,10 @@
 import { initializeApp } from 'firebase/app'
+import { 
+    getAuth,
+    signInWithPopup, 
+    signInWithRedirect,
+    GoogleAuthProvider 
+    } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBcUFaRa-R9n4P4FPuIScHR1xGlZf_rPVI",
@@ -9,4 +15,12 @@ const firebaseConfig = {
     appId: "1:470208784925:web:55938b29d39674b57a34f4"
   };
 
-  const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
