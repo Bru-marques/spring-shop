02b6c8db70c55e2f653dae1df2e7fc6e7156840e
bru-marques <brmarques1108@gmail.com>
2023-06-01 17:59:30 +0100

    Adding firebase and create firebase instance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home/home.component';
 import NavigationBar from './routes/navigation-bar/navigation-bar.component';
+import SignIn from './routes/SignIn/sign-in.component.tsx';
 
 const App = () => {
   return (
    <Routes>
     <Route path='/' element={<NavigationBar/>}>
       <Route index element={ <Home /> } />
+      <Route path='sign-in' element={ <SignIn /> } />
     </Route>
    </Routes>
   );
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBcUFaRa-R9n4P4FPuIScHR1xGlZf_rPVI",
+    authDomain: "spring-store-e0e38.firebaseapp.com",
+    projectId: "spring-store-e0e38",
+    storageBucket: "spring-store-e0e38.appspot.com",
+    messagingSenderId: "470208784925",
+    appId: "1:470208784925:web:55938b29d39674b57a34f4"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
