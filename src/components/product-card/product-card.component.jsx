d270cb1256d00398fe61ef933994c70c4a7c66b5
bru-marques <brmarques1108@gmail.com>
2023-06-03 20:30:43 +0100

    Add products data, shop page &  product component

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,19 @@
+import Button from 'components/button/button.component';
+import './product-card.styles.scss';
+
+const ProductCard = ({ product }) => {
+    const {imageUrl, name,  price} = product
+
+    return(
+        <div className='product-card-component'>
+            <img src={ imageUrl } alt={`${name}`}/>
+            <div className='footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <Button buttonType='invert'>Add to card</Button>
+        </div>
+    )
+}
+
+export default ProductCard
--- src/routes/navigation-bar/navigation-bar.component.jsx
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -13,7 +13,6 @@ const NavigationBar = () => {
         await signOutUser()
     }
 
-    console.log(currentUser)
     return (
         <>
             <div className="navigation">
